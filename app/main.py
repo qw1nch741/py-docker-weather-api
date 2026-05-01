4db945c1c21b72b1d1462b042c1b466644796e6b
Kyryl <sautakiki.com@gmail.com>
2026-05-01 18:29:00 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ import os
 
 def get_weather() -> None:
     api_key = os.getenv("API_KEY")
-    url = "http://api.weatherapi.com/v1/current.json"
+    URL = "http://api.weatherapi.com/v1/current.json"
+    CITY = "Paris"
 
-    params = {"key": api_key, "q": "Paris"}
-    response = requests.get(url, params=params)
+    PARAMS = {"key": api_key, "q": CITY}
+    response = requests.get(URL, params=PARAMS)
 
     data = response.json()
     print(f"Current weather in {data['location']['name']}: {data['current']['temp_c']}°C")
